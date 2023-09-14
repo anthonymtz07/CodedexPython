3f7a42ae5e4c12c68ed41ed2812c8c5ce8edee08
Jesus Antonio Martínez González <martinezgonzalezj808@gmail.com>
2023-09-13 23:24:44 -0600

    Volumen.2 -Avances

---
+++ Unidad 2/data_variables.py
@@ -0,0 +1,55 @@
+"""In programming, variables are used for storing data values. Each variable has a name and they hold a value. 
+The variables name can consist of letters, numbers, an the _ underscore character.
+These are all valid variable names and values: 
+
+name = 'Antonio Martinez'
+user_id = 2020143001
+progress = 0.75
+exp = 60
+verified = true
+
+The = equals sign means assigment: 
+    * We are assinging the strig value 'Antonio Martinez' to the variable name.
+    * We are assinging the number value 2020143001 to the variable user_id.
+    * We are assinging the number value 0.75 to the variable progress.
+    * We are assinging the number value 60 to the variable exp.
+    * We are assinging the truth value true to the variable verified.
+
+We can also change the value of a variable, or print it out: 
+xp = 70 
+xp = 80
+print(xp) #output: 80
+
+Here, we ara assigning the number value 70 to the variable xp. Then, we are reassigning to integer value 80 to the same variable. And printing it out. 
+
+Data types
+INTEGER
+An integer, or int, is a whole number. It has no decimal point and contains the number 0, positive and negative counting numbers. If we were counting the 
+number of people on the bus or the number of jellybeans in a jar, we would use an integer. 
+
+year = 2023
+age = 32
+
+FLOAT
+A floating-point number or a float, is a decimal number. It can be used to represent fractions or precise measeurements. If you were measuring the lenght 
+and width of the couch, calculating the test score percentage, or storing a baseball player's batting average, we would use a float instead of an int.
+
+pi = 3.14159
+meal_cost = 12.99
+
+STRING
+A string or str, is used for storing text. Strings are wrapped in double quotes " " or single quotes ' '.
+message = "good nine"
+user = "@sonnynommon"
+
+BOOLEAN
+A Boolean data type, or bool, stores a value that can only be either true or false. In Python, it's capitalized True or False. 
+It's named after the British mathematician George Boole
+
+late_to_class = False
+crackly = true.
+
+
+
+
+"""
